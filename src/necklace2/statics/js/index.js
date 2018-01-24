8764a1583b0a91a691492694c7297e85055119b9
iR <irwifi@gmail.com>
2018-01-24 13:20:52 +0545

    Necklace version 1 and 2

---
+++ src/necklace2/statics/js/index.js
@@ -0,0 +1,283 @@
+$(() => { 
+
+	// $.get("api_url", function(data, status){
+	let data = {
+		"pearl_type": [
+			{"name": "black", "thumb": "pearl_type/black_thumb.png", "price": 100},
+			{"name": "gold",  "thumb": "pearl_type/gold_thumb.png",  "price": 200},
+			{"name": "white", "thumb": "pearl_type/white_thumb.png", "price": 300}
+		], "pearl_color": [
+			{"name": "black", 		"type": "black", "thumb": "pearl_color/black_thumb.png",       "image": "pearl_color/black.png",       "price": 110},
+			{"name": "blue",  		"type": "black", "thumb": "pearl_color/blue_thumb.png",        "image": "pearl_color/blue.png",        "price": 120},
+			{"name": "green", 		"type": "black", "thumb": "pearl_color/green_thumb.png",       "image": "pearl_color/green.png",       "price": 130},
+			{"name": "green_light", "type": "black", "thumb": "pearl_color/green_light_thumb.png", "image": "pearl_color/green_light.png", "price": 140},
+			{"name": "gold", 		"type": "gold",  "thumb": "pearl_color/gold_thumb.png",        "image": "pearl_color/gold.png",        "price": 150},
+			{"name": "gold_light",  "type": "gold",  "thumb": "pearl_color/gold_light_thumb.png",  "image": "pearl_color/gold_light.png",  "price": 160},
+			{"name": "pink",   		"type": "white", "thumb": "pearl_color/pink_thumb.png",        "image": "pearl_color/pink.png",        "price": 170},
+			{"name": "white",  		"type": "white", "thumb": "pearl_color/white_thumb.png",       "image": "pearl_color/white.png",       "price": 180},
+			{"name": "silver", 		"type": "white", "thumb": "pearl_color/silver_thumb.png",      "image": "pearl_color/silver.png",      "price": 190}
+		], "pearl_size": [
+			{"name": "5.5", "label": "5.5 X 6mm", "thumb": "products/pearlsize1.png", "thumb_size": "17" ,"price": 110},
+			{"name": "6.0", "label": "6 X 6.5mm", "thumb": "products/pearlsize2.png", "thumb_size": "19" ,"price": 110},
+			{"name": "6.5", "label": "6.5 X 7mm", "thumb": "products/pearlsize3.png", "thumb_size": "21" ,"price": 110},
+			{"name": "7.0", "label": "7.5 X 8mm", "thumb": "products/pearlsize4.png", "thumb_size": "23" ,"price": 110},
+			{"name": "7.5", "label": "8 X 8.5mm", "thumb": "products/pearlsize5.png", "thumb_size": "25" ,"price": 110},
+			{"name": "8.5", "label": "8.5 X 9mm", "thumb": "products/pearlsize6.png", "thumb_size": "27" ,"price": 110}
+		], "necklace_length": [
+			{"name": "16", "label": "16\"", "price": 400},
+			{"name": "18", "label": "18\"", "price": 400},
+			{"name": "22", "label": "22\"", "price": 400},
+			{"name": "24", "label": "24\"", "price": 400},
+			{"name": "30", "label": "30\"", "price": 400},
+			{"name": "36", "label": "36\"", "price": 400}
+		], "pearl_quality": [
+			{"name": "AAA", "label": "AAA", "price": 110},
+			{"name": "AA",  "label": "AA",  "price": 120},
+			{"name": "A",   "label": "A",   "price": 130}
+		], "clasp": [
+			{"name": "dolor",   "label": "Dolor",   "thumb": "products/clasp/1.jpg", "price": 110},
+			{"name": "egestas", "label": "Egestas", "thumb": "products/clasp/2.jpg", "price": 120},
+			{"name": "sapien",  "label": "Sapien",  "thumb": "products/clasp/3.jpg", "price": 130}
+		], "ring_metal": [
+	]}
+
+		load_pearl_color(data.pearl_color);
+
+		load_btn(data.pearl_size, ".wrapper_pearl_size");
+		load_btn(data.pearl_type, ".wrapper_pearl_type");
+		load_btn(data.necklace_length, '.wrapper_length');
+		load_btn(data.pearl_quality, '.wrapper_pearl_quality');
+		load_btn(data.clasp, '.wrapper_clasp');
+
+
+
+		function load_pearl_color(pearl_color) {
+			for(item in pearl_color) {
+				$(".option_pearl_color.sample").clone().addClass("new").removeClass("sample hidden").appendTo(".wrapper_pearl_color");
+				$(".option_pearl_color.new").attr({"data-name": pearl_color[item].name, "data-type": pearl_color[item].type, "data-image": pearl_color[item].image}).find(".price_amount").text(pearl_color[item].price);
+				$(".option_pearl_color.new").find(".image.thumb").attr("src", "statics/img/" + pearl_color[item].thumb);
+				$(".option_pearl_color.new").removeClass("new")
+			}
+		}
+
+		function load_btn(data, place){// place = ".wrapper" (string)
+			for(item in data) {
+				$(place+" .sample").clone().addClass("new").removeClass("sample hidden").appendTo(place);
+				$(place+" .new").attr({"data-name": data[item].name,}).find(".price_amount").text(data[item].price);
+				$(place+" .new").find(".label").text(data[item].label);
+				var img_box = $(place+" .new").find(".image.thumb");
+				if(img_box.length) img_box.attr("src", "statics/img/" + data[item].thumb);
+				$(place+" .new").removeClass("new");
+				if(data[item].thumb_size) img_box.css({'width': data[item].thumb_size+'px', 'margin-top': data[5].thumb_size-data[item].thumb_size+'px'});
+			}
+		}
+
+		$(".item").click(function() {
+			$(this).parent().find(".item").removeClass('selected');
+			$(this).addClass('selected');
+			setPrice();	
+		});
+
+		$(".option_pearl_type").on("click", function() {
+			$("#colorsempty").hide();
+			$(".option_pearl_color").addClass("hidden");
+			$(".option_pearl_color[data-type='"+$(this).attr("data-name")+"']").removeClass("hidden");
+			$(".option_pearl_color:not(.hidden):first").trigger("click");
+		});
+
+		$(".option_pearl_size").on("click", function() {
+			setPearlSize($(this).data('value'));
+		});
+
+		$(".option_pearl_color").on("click", function() {
+			var url = "statics/img/" + $(this).attr("data-image");
+			var thumb = $('.image.thumb', this).attr("src");
+			$(".option_pearl_size .image.thumb").attr('src', thumb);
+			$("#body_outline").attr({"src": "statics/img/clear.png", "data-zoom-image": "statics/img/clear.png "}).data('type', 'with_body');
+			$("#pearl").attr({"src": url});
+			zoomReset();
+		});
+
+		$(".rondelle input").on("change", function(event) {
+			if($('#diamonds').prop("checked")) {
+				$('#extra').removeClass('hidden');
+			}
+			else {$('#extra').addClass('hidden');
+			$('#rondelle').addClass('hidden');}
+		});
+
+
+		$('.selectors a').on("click", function(event) {
+			event.preventDefault();
+			var color = $('.option_pearl_color.selected').data('name');
+			if($(this).data('type')=='with_body') var pearl = "statics/img/products/necklace_body/"+color+".jpg";
+			else var pearl = "statics/img/products/necklace/"+color+".jpg";
+
+			$(".photo #pearl").attr({"src": pearl});
+			$(".photo #body_outline").attr({"src": 'statics/img/clear.png', "data-zoom-image": 'statics/img/clear.png'}).data('type', $(this).data('type'));
+			$(".photo #clasp").attr('src', getUrlClasp());
+			zoomReset();
+		});
+
+		$(".option_rondelle").on("click", function() {
+
+		});
+
+		$(".option_clasp").on("click", function() {
+			$('#clasp').attr('src', getUrlClasp());
+			zoomReset();
+		});
+
+
+		$(".metal").on("click", function() {
+			$('#clasp').attr('src', getUrlClasp());
+			zoomReset();
+		});
+
+		setTimeout(() => {
+			$.each($('.option-list'), function(index, val) {
+				$(".item:not('.hidden'):first", $(val)).trigger("click");
+			});
+		}, 100);
+
+
+		$(".page1_link").on("click", () => {
+			toStep(1);
+			$("#body_outline").attr({"src": "statics/img/clear.png", "data-zoom-image": "statics/img/clear.png"}).data('type', 'with_body');
+			$(".photo #pearl").attr({"src": getUrlNecklace()});
+			$(".photo #clasp").attr('src', getUrlClasp());
+			$('.selectors a').addClass('hidden');
+			var size = $('.option_pearl_size.selected').data('value');
+			setPearlSize(size);
+			zoomReset();
+		});
+
+		$(".page2_link").on("click", () => {
+			toStep(2);
+		});
+
+
+		$("#page1_submit").on("click", () => {
+			toStep(2);
+			$('.selectors a').removeClass('hidden');
+			$('#pearl').css({width: '100%',	left: 0, marginTop: 0});
+			$(".photo #body_outline").attr({"src": "statics/img/clear.png", "data-zoom-image": "statics/img/clear.png"}).data('type', 'without_body');
+			$(".photo #pearl").attr({"src": getUrlNecklace()});
+			$('#clasp').attr('src', getUrlClasp());		
+			zoomReset();
+			$('html, body').animate({ scrollTop: $("#page2").offset().top }, 0);		
+		});
+
+
+		$("#page2_submit").on("click", () => {
+			toStep(3);
+			zoomReset();
+			$('html, body').animate({ scrollTop: $("#page3").offset().top }, 0);
+		});
+
+		$('.lightBox-link').simpleLightbox({showCounter:  true, widthRatio:  0.98, heightRatio: 0.98, loop: false});
+
+		$('.lightBox-link').on('shown.simplelightbox', function () {
+			var pearl = $('#pearl').attr('src');
+			var width = $('.simple-lightbox .sl-image img').width();
+			var bg = 'statics/img/clear.png';
+			if($('#page1').css('display')=='block'){
+				var clasp = 'statics/img/clear.png';
+			}else{
+				var clasp = $('#clasp').attr('src');
+			}
+
+			$('.simple-lightbox .sl-image').append('<img src="'+bg+'" style="width:'+width+'px;">');
+			$('.simple-lightbox .sl-image').append('<img src="'+pearl+'" style="width:'+width+'px;">');
+			$('.simple-lightbox .sl-image').append('<img src="'+clasp+'" style="width:'+width+'px;">');
+
+			$('.simple-lightbox .sl-image img').css('border', '1px solid #000');		
+		});
+
+		//$("[data-slider]").bind("slider:ready slider:changed", function (event, data) {
+		// 	$(this).data('value', data['value']);
+		// 	tone(data['value']);
+	    //});	
+
+		function zoomReset() {
+			$('.zoomContainer').remove();
+			$('.photo #body_outline').removeData('elevateZoom');
+			$('.photo #body_outline').elevateZoom({
+				zoomWindowWidth: 565,
+				zoomWindowHeight: 565,
+				zoomWindowOffetx: 70,
+				zoomWindowOffety: -31
+			});
+		}
+
+		function setPearlSize(value){
+			var width = value*6+49;
+			var margin = (100-width)/2;
+			$('#pearl').css({width: width +'%',	left: margin +'%', marginTop: margin/2 +'%'});
+		}
+
+		function setPrice(){
+			var price = 0;
+			$.each($('.item.selected .price_amount'), function(index, val) {
+				 price += $(val).text()*1;
+			});
+			price = price+'';
+			if (price > 3) {
+	   			price = price.replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+	  		}
+			$('.build-price .amount').text(price);
+		}
+		function toStep(step){// step [number]
+			$(".page").not("#page"+step).hide();		
+			$("#page"+step).show();
+
+			$(".separator").removeClass('active-left active-right');
+			$(".separator").eq(step-1).addClass('active-left');
+			if(step > 1) $(".separator").eq(step-2).addClass('active-right');
+			
+			$(".build-steps .step").removeClass('active inactive');
+			switch (step) {
+			   case 1: $(".step.first").addClass("active"); $(".step.second").add(".step.last").addClass("inactive");  break;
+			   case 2: $(".step.second").addClass("active"); $(".step.last").addClass("inactive"); break;
+			   case 3: $(".step.last").addClass("active"); break;
+			   default: break;
+			}
+			$(".step."+step).addClass("active");
+		}
+
+});
+
+/*
+function tone(value){
+    value = 1.125-value/4;
+    $('#pearl').add('.zoom_ring_image').css({
+         'filter'         : 'brightness('+value+')',
+         '-webkit-filter' : 'brightness('+value+')',
+         '-moz-filter'    : 'brightness('+value+')',
+         '-o-filter'      : 'brightness('+value+')',
+         '-ms-filter'     : 'brightness('+value+')'
+    }); 
+}*/
+
+function getUrlNecklace(){
+	var color = $('.option_pearl_color.selected').data('name');
+	if($('#page1').css('display')=='block')	var url = 'statics/img/pearl_color/'+color+'.png';
+	else if($('#body_outline').data('type')=="with_body"){
+			var url = 'statics/img/products/necklace_body/'+color+'.jpg';
+		}else{
+			var url = 'statics/img/products/necklace/'+color+'.jpg';
+		}		
+	return url;
+}
+
+
+function getUrlClasp(){	
+	if($('#page1').css('display')=='block' || $('#body_outline').data('type')=="with_body")	{
+		var url = 'statics/img/clear.png';
+	}else {
+		var clasp = $('.option_clasp.selected').data('name');
+		var metal_color = $('.metal.selected').data('color');
+		var url = 'statics/img/products/clasp/'+clasp+'_'+metal_color+'.png';
+	}
+	return url;
+}
